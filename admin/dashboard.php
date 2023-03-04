9ee2eba50a94dfd9bb49fd9fe0b6da38a8b46ded
IKSopnil <sopnil4o4o@gmail.com>
2023-03-04 21:08:05 +0600

    9.07pm

---
+++ admin/dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Dashboard</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+</head>
+
+
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Admin Dashboard</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Users</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Products</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Orders</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- Content -->
+	<div class="container-fluid mt-3">
+		<h1>Dashboard</h1>
+		<p>Welcome to the admin dashboard!</p>
+	</div>
+	<!-- Bootstrap JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- offer.php
+++ offer.php
@@ -1,7 +1,10 @@
 <head>
+    <!--boostrap-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  
+    <!--icon font awesomr-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+
 </head>
 <?php include "db_connect.php";
 
@@ -61,13 +64,16 @@ $result = $conn->query($sql);
             ?>
                     <div class=" col-lg-4 d-flex justify-content-center align-item-center ">
                         <div class="card h-100 text-center ">
+                        
                             <img src="<?php echo $row['image'] ?>" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $row['title'] ?></h5>
                                 <p class="card-text"><?php echo $row['description'] ?></p>
                             </div>
                             <div class="card-footer">
-                                <small ><?php echo $row['price'] ?></small>
+                            <p class="fas fa-tags text-start mx-auto d-flex justify-content-end "><i class="mx-1" ></i><?php echo $row['off_percentage'] ?> <span class="mx-1"> % OFF</span></p>
+                                <p style="font-weight:bold" class=" justify-content-cente"><?php echo $row['price'] ?> Taka ( &#2547; )</p>
+                                <a href="#" class="btn btn-outline-dark   d-flex justify-content-center ">Add to Cart</a>
                             </div>
                         </div>
                     </div>
