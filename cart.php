d92d708b5b08398c3eb3e6dfd1e0cf1890e269d1
IKSopnil <sopnil4o4o@gmail.com>
2023-03-10 11:20:26 +0600

    cart

--- cart.php
+++ cart.php
@@ -8,11 +8,26 @@ session_start();
 
 if(isset($_POST["add_to_cart"])){
 
-if(isset($_SESSION["cart"])){
-  $session_array_id= array_column($_SESSION["cart"], 'id');
-
-  if(!in_array($_GET['id'],$session_array_id)){
-
+  if(isset($_SESSION["cart"])){
+  
+    $session_array_id= array_column($_SESSION["cart"], 'id');
+  
+    if(!in_array($_GET['id'],$session_array_id)){
+  
+      $session_array = array(
+        'id'=> $_GET['id'],
+        'image'=> $_POST['image'],
+        'title'=> $_POST['title'],
+        'price'=> $_POST['price'],
+    
+      );
+  
+      $_SESSION['cart'][]=$session_array;
+    }
+  
+  }
+  else{
+  
     $session_array = array(
       'id'=> $_GET['id'],
       'image'=> $_POST['image'],
@@ -20,27 +35,11 @@ if(isset($_SESSION["cart"])){
       'price'=> $_POST['price'],
   
     );
-
+  
+    $_SESSION['cart'][]=$session_array;
   }
-
-}
-else{
-
-  $session_array = array(
-    'id'=> $_GET['id'],
-    'image'=> $_POST['image'],
-    'title'=> $_POST['title'],
-    'price'=> $_POST['price'],
-
-  );
-
-  $_SESSION['cart'][]=$session_array;
-}
-
-}
-
-
-?>
+  
+?>  
 
 <head>
 
@@ -84,69 +83,54 @@ else{
     </div>
     <div class="container cart ">
       <h2 class="cart-header py-4">Your Order</h2>
-     <?php 
-       if(!empty($_SESSION['cart'])){
-
-       foreach ($_SESSION['cart'] as $key => $value){
 
 
-       
-       ?>
-      <div class="row cart-items">
-        <div class="col d-flex justify-content-start mx-lg-5 my-lg-5">
-          <img src="<?php echo $value['image'] ?>" height="50" width="50" alt="Item 1" class="cart-item-img">
-          <div class="mx-4">
-            <h3 class=""><?php echo $value['title'] ?></h3>
-          </div>
-        </div>
-        <div class="col d-flex justify-content-end mx-lg-5 my-lg-5">
-          <div class="">
-            <label for="item1-quantity">Quantity:</label>
-            <input type="number" id="item1-quantity" name="item1-quantity" min="1" max="10" value="1" data-price="0">
-          </div>
-          <div class="">
-            <p class="mx-1">  ৳ <?php echo $value['price'] ?></p>
-          </div>
-          <div class="mx-4">
-            <button class="btn btn-danger btn-sm remove-item-btn">Remove</button>
-          </div>
+      <?php 
+
+$total_price = 0;
+
+if(!empty($_SESSION['cart'])){
+
+  foreach ($_SESSION['cart'] as $key => $value){
+
+    $total_price += $value['price'];
+    ?>
+    <div class="row cart-items">
+      <div class="col d-flex justify-content-start mx-lg-5 my-lg-5">
+        <img src="<?php echo $value['image'] ?>" height="50" width="50" alt="Item 1" class="cart-item-img">
+        <div class="mx-4">
+          <h3 class=""><?php echo $value['title'] ?></h3>
         </div>
       </div>
-      <div class="cart-total my-4">
-        <h4 class="cart-total-title mb-3">Total: ৳ 0</h4>
-        <button class="btn  btn-outline-success mb-3">Confirm Order</button>
+      <div class="col d-flex justify-content-end mx-lg-5 my-lg-5">
+        <div class="">
+          <label for="item1-quantity">Quantity:</label>
+          <input type="number" id="item1-quantity" name="item1-quantity" min="1" max="10" value="1" data-price="0">
+        </div>
+        <div class="">
+          <p class="mx-1">  ৳ <?php echo $value['price'] ?></p>
+        </div>
+        <div class="mx-4">
+          <button class="btn btn-danger btn-sm remove-item-btn">Remove</button>
+        </div>
       </div>
     </div>
     <?php 
-       }
-      }
+  }
+}
+?>
 
-       
-       ?>
-    <script>
-      // Get the quantity input elements and the total price element
-      const quantityInputs = document.querySelectorAll('input[type="number"]');
-      const totalPriceElement = document.querySelector('.cart-total-title');
+<div class="cart-total my-4">
+  <h4 class="cart-total-title mb-3">Total: ৳ <?php echo $total_price ?></h4>
+  <button class="btn  btn-outline-success mb-3">Confirm Order</button>
+</div>
 
-      // Loop through the quantity input elements and add an event listener to each one
-      quantityInputs.forEach(input => {
-        input.addEventListener('input', () => {
-          // Get the price of the item
-          const price = parseFloat(input.dataset.price);
-
-          // Calculate the new total price
-          let totalPrice = 0;
-          quantityInputs.forEach(input => {
-            totalPrice += parseFloat(input.value) * parseFloat(input.dataset.price);
-          });
-
-          // Update the total price element with the new total price
-          totalPriceElement.textContent = 'Total: ৳ ' + totalPrice.toFixed(2);
-        });
-      });
-    </script>
+  
 
   </div>
 </section>
+<?php
+ }
+ ?>
 
 <?php include "footer.php" ?>
