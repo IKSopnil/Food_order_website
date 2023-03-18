8814624f694a4c65d66c055fab7633057e2d34e8
IKSopnil <sopnil4o4o@gmail.com>
2023-03-18 19:59:50 +0600

    add post success

--- admin/addpost.php
+++ admin/addpost.php
@@ -26,79 +26,76 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['inputState'])) {
     $title = $_POST['title'];
     $price = $_POST['price'];
     $description = $_POST['description'];
-    $discount = ($category === 'offer') ? $_POST['discount'] : null;
+    $discount = $_POST['discount'];
     $uploadDir = 'img_upload/';
 
-    if(isset($_FILES['file']['name']) && $_FILES['file']['name'] != ''){
+    if (isset($_FILES['file']['name']) && $_FILES['file']['name'] != '') {
         // handle file upload
         $uploadFile = $uploadDir . basename($_FILES['file']['name']);
         if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
 
 
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            if ($category === "appetizer") {
+                $sql = "INSERT INTO `appetizers`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            } elseif ($category === "maincourse") {
+                $sql = "INSERT INTO `main_courses`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            } elseif ($category === "dessert") {
+                $sql = "INSERT INTO `desserts`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            } elseif ($category === "beverage") {
+                $sql = "INSERT INTO `beverages`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            } elseif ($category === "offer") {
+                $sql = "INSERT INTO `offer`(`image`, `title`, `description`, `price`,`off_percentage`) VALUES ('$uploadFile','$title','$description','$price','$discount')";
+            }
+            // Insert data into database
+            if ($conn->query($sql) === TRUE) {
+                echo '<div class="alert alert-success" role="alert">
+        Your post has been successfully posted!
+    </div>';
+                echo '<meta http-equiv="refresh" content="1;url=addpost.php">';
+                exit();
+            } else {
+                echo '<div class="alert alert-danger" role="alert">
+                  Error inserting data: ' . $conn->error . '
+                </div>';
+            }
+        } else {
+        }
+    } elseif (isset($_POST['image_url']) && $_POST['image_url'] != '') {
+        // handle image URL
+        $image_url = $_POST['image_url'];
+        // code to download image from URL and save it to $uploadDir
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
         if ($category === "appetizer") {
-            $sql = "INSERT INTO `appetizers`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            $sql = "INSERT INTO `appetizers`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
         } elseif ($category === "maincourse") {
-            $sql = "INSERT INTO `main_courses`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            $sql = "INSERT INTO `main_courses`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
         } elseif ($category === "dessert") {
-            $sql = "INSERT INTO `desserts`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            $sql = "INSERT INTO `desserts`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
         } elseif ($category === "beverage") {
-            $sql = "INSERT INTO `beverages`(`image`, `title`, `description`, `price`) VALUES ('$uploadFile','$title','$description','$price')";
+            $sql = "INSERT INTO `beverages`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
         } elseif ($category === "offer") {
-            $sql = "INSERT INTO `offer`(`image`, `title`, `description`, `price`,`off_percentage`) VALUES ('$uploadFile','$title','$description','$price','$discount')";
+            $sql = "INSERT INTO `offer`(`image`, `title`, `description`, `price`,`off_percentage`) VALUES ('$image_url','$title','$description','$price','$discount')";
         }
         // Insert data into database
         if ($conn->query($sql) === TRUE) {
             echo '<div class="alert alert-success" role="alert">
-        Your post has been successfully posted!
-    </div>';
+     Your post has been successfully posted!
+ </div>';
             echo '<meta http-equiv="refresh" content="1;url=addpost.php">';
             exit();
         } else {
             echo '<div class="alert alert-danger" role="alert">
-                  Error inserting data: ' . $conn->error . '
-                </div>';
+               Error inserting data: ' . $conn->error . '
+             </div>';
         }
-    }
-    else{
-
-    }
-}
-elseif(isset($_POST['image_url']) && $_POST['image_url'] != ''){
-    // handle image URL
-    $image_url = $_POST['image_url'];
-    // code to download image from URL and save it to $uploadDir
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    if ($category === "appetizer") {
-        $sql = "INSERT INTO `appetizers`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
-    } elseif ($category === "maincourse") {
-        $sql = "INSERT INTO `main_courses`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
-    } elseif ($category === "dessert") {
-        $sql = "INSERT INTO `desserts`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
-    } elseif ($category === "beverage") {
-        $sql = "INSERT INTO `beverages`(`image`, `title`, `description`, `price`) VALUES ('$image_url','$title','$description','$price')";
-    } elseif ($category === "offer") {
-        $sql = "INSERT INTO `offer`(`image`, `title`, `description`, `price`,`off_percentage`) VALUES ('$image_url','$title','$description','$price','$discount')";
-    }
-    // Insert data into database
-    if ($conn->query($sql) === TRUE) {
-        echo '<div class="alert alert-success" role="alert">
-    Your post has been successfully posted!
-</div>';
-        echo '<meta http-equiv="refresh" content="1;url=addpost.php">';
-        exit();
     } else {
-        echo '<div class="alert alert-danger" role="alert">
-              Error inserting data: ' . $conn->error . '
-            </div>';
+        // no image provided
     }
-} else {
-    // no image provided
-}
 }
 ?>
 
@@ -127,7 +124,7 @@ elseif(isset($_POST['image_url']) && $_POST['image_url'] != ''){
                     <label for="price">Price:<span class=""></span></label>
                     <input type="number" required placeholder="৳ ৳" class="form-control" name="price" />
                 </div>
-                <div class="form-group" id="discount_percentage" style="display:none;">
+                <div class="form-group" id="discount_percentage">
                     <label for="discount">Discount percentage (only for offer):<span class=""></span></label>
                     <input type="number" required placeholder="offer percentage %" class="form-control" name="discount" />
                 </div>
@@ -137,42 +134,30 @@ elseif(isset($_POST['image_url']) && $_POST['image_url'] != ''){
                 </div>
                 <div id="footer-left">
                     <label for="file_choose">Upload Image:</label><br>
-                    <input  type="file" name="file" id="file" class="inputfile" accept='' onchange="fileChange(this)" />
+                    <input type="file" name="file" id="file" class="inputfile" accept='' onchange="fileChange(this)" />
                     <label for="file"><i class="fas fa-paperclip"></i></label>
                 </div>
                 <div id="paste-img">
                     <label for="image_url">Or Paste Image URL:</label><br>
                     <input type="text" name="image_url" id="image_url" />
                 </div>
-                
-<script>
-    function checkForm() {
-        var fileInput = document.getElementById("file");
-        var urlInput = document.getElementById("image_url");
-        if (fileInput.value == "" && urlInput.value == "") {
-            alert("Please upload an image or paste an image URL.");
-            return false;
-        }
-        return true;
-    }
-</script>
+
+                <script>
+                    function checkForm() {
+                        var fileInput = document.getElementById("file");
+                        var urlInput = document.getElementById("image_url");
+                        if (fileInput.value == "" && urlInput.value == "") {
+                            alert("Please upload an image or paste an image URL.");
+                            return false;
+                        }
+                        return true;
+                    }
+                </script>
                 <div class="form-group my-4 ">
-                    <button type="submit" class="btn btn-success mx-4 px-4">Create</button>
+                    <button type="submit" value="submit" class="btn btn-success mx-4 px-4">Create</button>
                     <button type="reset" class="btn btn-danger px-4">Reset</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-
-<script>
-    $(document).ready(function() {
-        $("#inputState").change(function() {
-            if ($(this).val() === "offer") {
-                $("#discount_percentage").show();
-            } else {
-                $("#discount_percentage").hide();
-            }
-        });
-    });
-</script>
