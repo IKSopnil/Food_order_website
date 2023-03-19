0f1641c9290446a25f80aa1d2c04dc4753b37520
IKSopnil <sopnil4o4o@gmail.com>
2023-03-19 14:35:31 +0600

    1

--- admin/panelnavbar.php
+++ admin/panelnavbar.php
@@ -10,6 +10,9 @@ if (!isset($_SESSION['username'])) {
 
 <head>
 	<title>Admin Dashboard</title>
+	 <!--font awesome-->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 	<!--bootstrap-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -55,18 +58,25 @@ if (!isset($_SESSION['username'])) {
 
 				</li>
 			</ul>
-
-			<ul class="navbar-nav ms-auto">
+			<ul class="navbar-nav mx-5 px-4">
 				<?php if (isset($_SESSION['username'])) { ?>
-					<li class="nav-item">
-						<a class="nav-link" href="logout.php">Logout</a>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						<i class="far fa-user-circle"></i>
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<li><a class="dropdown-item" href="#"></a></li>
+							<li>
+								<hr class="dropdown-divider">
+							</li>
+							<li><a class="dropdown-item" href="logout.php">Logout</a></li>
+						</ul>
 					</li>
-				<?php } else { ?>
-					<li class="nav-item">
-						<a class="nav-link" href="login.php">Login</a>
-					</li>
-				<?php } ?>
+				<?php
+			 } 
+			 ?>
 			</ul>
+
 
 		</div>
 
