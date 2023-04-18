7ffca721b876f0585324933359d52dabb1178ce2
IKSopnil <sopnil4o4o@gmail.com>
2023-04-19 01:45:57 +0600

    dashboard desgn

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['admin_username'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Admin Dashboard</title>
   <!-- Bootstrap CSS -->
@@ -21,52 +22,56 @@ if (!isset($_SESSION['admin_username'])) {
 <?php include "panelnavbar.php" ?>
 <!-- Content -->
 <section>
-<div class="container-fluid mt-5 text-center">
-  <h1>Dashboard</h1>
-  <p>Welcome to the admin dashboard!</p>
-</div>
+  <div class="container-fluid mt-5 text-center">
+    <h1>Dashboard</h1>
+    <p>Welcome to the admin dashboard!</p>
+  </div>
 </section>
-<section>
-<div class="container text-center pt-5 ">
+<section class="container  text-center pt-5 ">
   <div class="row my-5">
-    <div class="col-5 card mx-5 p-5 clickable">
+    <div class="col-5 card mx-5 p-5 clickable" id="revenue">
       <h2>Revenue</h2>
     </div>
-    <div class="col-5 card mx-5 p-5 clickable">
+    <div class="col-5 card mx-5 p-5 clickable" id="accepted-order">
       <h2>Accepted Order</h2>
     </div>
   </div>
   <div class="row my-5">
-    <div class=" col-5  card mx-5 p-5 clickable">
+    <div class="col-5 card mx-5 p-5 clickable" id="new-user">
       <h2>New User</h2>
     </div>
-    <div class=" col-5  card mx-5 p-5 clickable">
+    <div class="col-5 card mx-5 p-5 clickable" id="message">
       <h2>Message</h2>
     </div>
   </div>
-</div>
+
 </section>
-  <style>
-    .card {
-      background-color: #fff;
-      border-radius: 4px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-      padding: 20px;
-      margin-bottom: 20px;
-    }
-    h2 {
-      margin-top: 0;
-      font-size: 24px;
-      font-weight: 600;
-    }
-    
+<style>
+
+  .card {
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin-bottom: 20px;
+  }
+
+  h2 {
+    margin-top: 0;
+    font-size: 24px;
+    font-weight: 600;
+  }
+
   .clickable {
-  cursor: pointer;
-  transition: transform 0.3s ease-out;
-}
-  </style>
+    cursor: pointer;
+    transition: transform 0.3s ease-out;
+  }
+  .clickable:hover {
+    cursor: pointer;
+    transform:scale(1.1);
+    color: #FA8F8F;
+  }
 
-
-
+</style>
 
 </html>
