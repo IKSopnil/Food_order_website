bad632f12030d729bbf3add25e35b00c0d1d15a8
IKSopnil <sopnil4o4o@gmail.com>
2023-04-29 21:57:41 +0600

    optimized

--- catagory/appetizer.php
+++ catagory/appetizer.php
@@ -53,39 +53,28 @@ if (!isset($_SESSION['cart'])) {
 $num_items_in_cart = count($_SESSION['cart']);
 
 
-
 ?>
 
 <head>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-
     <style>
         .card img {
             width: 100% !important;
             height: 250px;
-
         }
-
         .card {
             width: 501px;
             height: 300px;
             margin-left: 5%;
             box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
             transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
-
         }
-
         .card:hover {
             transform: scale(1.006, 1.006);
             transition: .5s;
             box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-
-
         }
     </style>
 </head>
@@ -96,7 +85,6 @@ $num_items_in_cart = count($_SESSION['cart']);
     </div>
     <div id="initiated" class="apitizer_card text-center   mx-auto">
         <div class="container row d-flex justify-content-center align-item-center  row-cols-md-3 g-4 ">
-
             <?php
             $items_per_page = 12;
             $total_pages = ceil($result->num_rows / $items_per_page);
@@ -104,14 +92,11 @@ $num_items_in_cart = count($_SESSION['cart']);
             $offset = ($current_page - 1) * $items_per_page;
             $sql = "SELECT * FROM appetizers LIMIT $items_per_page OFFSET $offset";
             $result = $conn->query($sql);
-
-
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
             ?>
                     <div class=" col-lg-4 d-flex justify-content-center align-item-center ">
                         <div class="card h-100 text-center ">
-
                             <img src="<?php echo $row['image'] ?>" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $row['title'] ?></h5>
@@ -132,9 +117,7 @@ $num_items_in_cart = count($_SESSION['cart']);
                 }
             }
             ?>
-
         </div>
-
         <?php
         echo '<div class="pagination my-5 justify-content-center" id="pagination">';
         for ($i = 1; $i <= $total_pages; $i++) {
@@ -142,10 +125,7 @@ $num_items_in_cart = count($_SESSION['cart']);
             echo '<a href="?page=' . $i . '" class="mx-3 ' . $active . '">' . $i . '</a>';
         }
         echo '</div>';
-
-
         ?>
-
 <script>
             function ajaxPaging() {
                 $('.pagination a').on('click', function(e) {
@@ -162,7 +142,6 @@ $num_items_in_cart = count($_SESSION['cart']);
             }
             ajaxPaging();
         </script>
-
         <style>
             .pagination {
                 display: flex;
@@ -180,14 +159,10 @@ $num_items_in_cart = count($_SESSION['cart']);
                 transition: background-color 0.3s;
                 margin-right: 5px;
             }
-
             .pagination a.active,
             .pagination a:hover {
                 background-color: #ddd;
             }
         </style>
-
-
     </div>
-
 </div>
--- catagory/beverage.php
+++ catagory/beverage.php
@@ -1,52 +1,28 @@
 <?php include "db_connect.php";
-
-
 $sql = "SELECT * FROM beverages";
-
-
 $result = $conn->query($sql);
-
 $sql = "SELECT * FROM beverages";
-
-
 $result = $conn->query($sql);
-
-
-
-
-
-
-
 $result = $conn->query($sql);
-
 if (isset($_POST["add_to_cart"])) {
-
     if (isset($_SESSION["cart"])) {
-
         $session_array_id = array_column($_SESSION["cart"], 'id');
-
         if (!in_array($_GET['id'], $session_array_id)) {
-
             $session_array = array(
                 'id' => $_GET['id'],
                 'image' => $_POST['image'],
                 'title' => $_POST['title'],
                 'price' => $_POST['price'],
-
             );
-
             $_SESSION['cart'][] = $session_array;
         }
     } else {
-
         $session_array = array(
             'id' => $_GET['id'],
             'image' => $_POST['image'],
             'title' => $_POST['title'],
             'price' => $_POST['price'],
-
         );
-
         $_SESSION['cart'][] = $session_array;
     }
 }
@@ -56,17 +32,12 @@ if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = array();
 }
 $num_items_in_cart = count($_SESSION['cart']);
-
-
-
 ?>
-
 
 <style>
     .card img {
         width: 100% !important;
         height: 250px;
-
     }
 
     .card {
@@ -82,66 +53,48 @@ $num_items_in_cart = count($_SESSION['cart']);
         transform: scale(1.006, 1.006);
         transition: .5s;
         box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-
-
     }
 </style>
-
 <div class="carousel-item mb-4" id="Beverages">
     <div class="text-center">
         <h3>Beverages</h3>
         <p>This category can include non-alcoholic drinks, such as cocktails, smoothies, coffee, and tea.</p>
-
-
     </div>
     <div class="drink" id="initiated_beverage">
         <div class="row container d-flex justify-content-center align-item-center row-cols-1 row-cols-md-3 g-4">
-
-
             <?php
-
             $items_per_page = 12;
             $total_pages = ceil($result->num_rows / $items_per_page);
             $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
             $offset = ($current_page - 1) * $items_per_page;
             $sql = "SELECT * FROM beverages LIMIT $items_per_page OFFSET $offset";
             $result = $conn->query($sql);
-
-
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
             ?>
                     <div class=" col-lg-4 d-flex justify-content-center align-item-center ">
                         <div class="card h-100 text-center ">
-
                             <img src="<?php echo $row['image'] ?>" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $row['title'] ?></h5>
                                 <p class="card-text"><?php echo $row['description'] ?></p>
                             </div>
                             <div class="card-footer">
-
                                 <p style="font-weight:bold" class=" justify-content-cente"><?php echo $row['price'] ?> Taka ( &#2547; )</p>
                                 <form action="index.php?id=<?= $row['id'] ?>" method="post">
                                     <button name="add_to_cart" class="btn btn-outline-dark justify-content-center w-100" type="submit">Add to Cart</button>
                             </div>
-
                             <input type="hidden" name="image" value="<?php echo $row['image'] ?>">
                             <input type="hidden" name="title" value="<?php echo $row['title'] ?>">
                             <input type="hidden" name="price" value="<?php echo $row['price'] ?>">
-
                             </form>
-
-
                         </div>
                     </div>
             <?php
                 }
             }
             ?>
-
         </div>
-
         <?php
         echo '<div class="pagination4 my-5 justify-content-center" id="pagination4">';
         for ($i = 1; $i <= $total_pages; $i++) {
@@ -166,8 +119,6 @@ $num_items_in_cart = count($_SESSION['cart']);
             }
             ajaxPaging();
         </script>
-
-
         <style>
             .pagination4 {
                 display: flex;
@@ -191,6 +142,5 @@ $num_items_in_cart = count($_SESSION['cart']);
                 background-color: #ddd;
             }
         </style>
-
     </div>
 </div>
